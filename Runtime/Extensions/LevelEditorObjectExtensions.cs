5eddfad0e657aac1f4327c0fd331c6eb33f33b52
Hertzole <hampusringstrom@gmail.com>
2021-07-21 13:09:30 +0200

    Extension for exposed fields.
    Added extension for level editor objects to easily check if it has any exposed fields.

---
+++ Runtime/Extensions/LevelEditorObjectExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Hertzole.ALE
+{
+	public static partial class LevelEditorExtensions
+	{
+		public static bool HasExposedProperties(this ILevelEditorObject obj)
+		{
+			IExposedToLevelEditor[] components = obj.GetExposedComponents();
+			if (components == null || components.Length == 0)
+			{
+				return false;
+			}
+
+			for (int i = 0; i < components.Length; i++)
+			{
+				if (components[i].HasVisibleFields)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
