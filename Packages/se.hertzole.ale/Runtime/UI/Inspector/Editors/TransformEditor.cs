49fb20cf2a1c83af093c224ef1ce31b22e79db57
Hertzole <hampusringstrom@gmail.com>
2022-05-01 23:30:48 +0200

    Fixed missing constructors

--- Packages/se.hertzole.ale/Runtime/UI/Inspector/Editors/TransformEditor.cs
+++ Packages/se.hertzole.ale/Runtime/UI/Inspector/Editors/TransformEditor.cs
@@ -1,33 +1,31 @@
 ﻿using UnityEngine;
+using UnityEngine.Scripting;
 
 namespace Hertzole.ALE
 {
-    public class TransformEditor : LevelEditorComponentEditor<Transform>
-    {
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-        private static void RegisterEditor()
-        {
-            AddEditor<Transform, TransformEditor>();
-        }
+	public class TransformEditor : LevelEditorComponentEditor<Transform>
+	{
+		[Preserve]
+		public TransformEditor() { }
 
-        public override void BuildUI()
-        {
-            //TODO: Snapping
-            //TODO: Make into 2D if 2D is enabled.
-            if (TryGetField<Vector3>(GetProperty(0), out LevelEditorInspectorField positionField))
-            {
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+		private static void RegisterEditor()
+		{
+			if (ALESettings.Get().ApplyTransformWrapper)
+			{
+				AddEditor<Transform, TransformEditor>();
+			}
+		}
 
-            }
+		public override void BuildUI()
+		{
+			//TODO: Snapping
+			//TODO: Make into 2D if 2D is enabled.
+			if (TryGetField<Vector3>(GetProperty(0), out LevelEditorInspectorField positionField)) { }
 
-            if (TryGetField<Vector3>(GetProperty(1), out LevelEditorInspectorField rotationField))
-            {
+			if (TryGetField<Vector3>(GetProperty(1), out LevelEditorInspectorField rotationField)) { }
 
-            }
-
-            if (TryGetField<Vector3>(GetProperty(2), out LevelEditorInspectorField scaleField))
-            {
-
-            }
-        }
-    }
+			if (TryGetField<Vector3>(GetProperty(2), out LevelEditorInspectorField scaleField)) { }
+		}
+	}
 }
