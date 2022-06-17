61de218fd11defe9dbb23639ccb42fba7c26e4a9
Hertzole <hampusringstrom@gmail.com>
2022-06-17 13:29:00 +0200

    Fixed custom generics not being included in IL2CPP

---
+++ Packages/se.hertzole.ale/CodeGen/Helpers/CodeGenLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Hertzole.ALE.CodeGen
+{
+	public static class CodeGenLogger
+	{
+		[Conditional("ALE_DEBUG")]
+		public static void Log(string message)
+		{
+			Console.WriteLine(message);
+		}
+	}
+}
